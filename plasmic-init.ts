c5ef4b3267cb099a48e6142ca16c0557b1cd070e
BostonRohan <bostonrohan@gmail.com>
2023-03-31 16:45:12 -0400

    init commit

---
+++ plasmic-init.ts
@@ -0,0 +1,21 @@
+import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+
+export const PLASMIC = initPlasmicLoader({
+  projects: [
+    {
+      id: process.env.NEXT_PUBLIC_PLASMIC_ID ?? "",
+      token: process.env.NEXT_PUBLIC_PLASMIC_TOKEN ?? "",
+    },
+  ],
+
+  preview: process.env.NODE_ENV === "development",
+});
+
+// You can register any code components that you want to use here; see
+// https://docs.plasmic.app/learn/code-components-ref/
+// And configure your Plasmic project to use the host url pointing at
+// the /plasmic-host page of your nextjs app (for example,
+// http://localhost:3000/plasmic-host).  See
+// https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
+
+// PLASMIC.registerComponent(...);
