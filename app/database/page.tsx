03ad8f967c6eb24fe602b9ad662a892ae3ff6763
BostonRohan <bostonrohan@gmail.com>
2023-06-23 16:36:38 -0400

    setup revalidation on videos request

--- app/database/page.tsx
+++ app/database/page.tsx
@@ -2,6 +2,9 @@ async function getVideos() {
   const res = await fetch(
     `https://studio.plasmic.app/api/v1/cms/databases/${process.env.NEXT_PUBLIC_CMS_ID}/tables/videos/query`,
     {
+      ...(process.env.VIDEOS_CACHE_TAG && {
+        next: { tags: [process.env.VIDEOS_CACHE_TAG] },
+      }),
       headers: {
         "x-plasmic-api-cms-tokens": `${process.env.NEXT_PUBLIC_CMS_ID}:${process.env.NEXT_PUBLIC_CMS_PUBLIC_TOKEN}`,
       },
