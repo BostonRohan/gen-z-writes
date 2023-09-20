1a86df2c93f67f9c94bc3aab3620e4e9d1a16d53
BostonRohan <bostonrohan@gmail.com>
2023-09-19 22:29:19 -0400

    adjust sitemap config and postbuild command

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,16 +1,36 @@
+const exclude = [
+  "/forgot-password",
+  "/profile",
+  "/studio/*",
+  "/user/*",
+  "/login",
+  "/signup",
+  "/profile",
+];
+
+// Save crawling budget by not fetching SSG meta files
+const NEXT_SSG_FILES = [
+  "/*.json$",
+  "/*_buildManifest.js$",
+  "/*_middlewareManifest.js$",
+  "/*_ssgManifest.js$",
+  "/*.js$",
+];
+
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
+const config = {
   siteUrl: "https://www.projectgenzwrites.com",
   generateRobotsTxt: true,
-  sitemapSize: 7000,
-  changefreq: "weekly",
-  exclude: [
-    "/forgot-password",
-    "/profile",
-    "/studio/*",
-    "/user/*",
-    "/login",
-    "/signup",
-    "/profile",
-  ],
+  exclude,
+  additionalPaths: ["/api/server-sitemap-index.xml"],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        disallow: NEXT_SSG_FILES,
+      },
+    ],
+  },
 };
+
+export default config;
