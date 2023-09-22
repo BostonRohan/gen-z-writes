f0b2659c582523f00ccc9fa050547e6418f5b059
BostonRohan <bostonrohan@gmail.com>
2023-09-22 17:54:58 -0400

    add flex wrap to contributors section

--- components/home/Contributors.tsx
+++ components/home/Contributors.tsx
@@ -5,11 +5,10 @@ export default function Contributors() {
   return (
     <section className="pt-80 px-4">
       <h2
-        className={`${bebasNeue.className} xs:text-[100px] sm:leading-none text-[20vw] leading-[20vw] text-center text-slate-200 mb-4`}
-      >
+        className={`${bebasNeue.className} xs:text-[100px] sm:leading-none text-[20vw] leading-[20vw] text-center text-slate-200 mb-4`}>
         Contributors
       </h2>
-      <section className="flex justify-center gap-16">
+      <section className="flex justify-center gap-16 flex-wrap">
         <Contributor name="Ruby Seidner" src="/ruby.webp" />
 
         <Contributor
@@ -18,7 +17,7 @@ export default function Contributors() {
           src="/boston.jpg"
         />
       </section>
-      <section className="flex justify-center mt-16 gap-16">
+      <section className="flex justify-center [@media(min-width:445px)]:mt-16 mt-32 gap-16 flex-wrap">
         <Contributor name="Randi Seidner" src="/randi.webp" />
         <Contributor name="Shannan Johnson" src="/shannan.jpeg" />
       </section>
