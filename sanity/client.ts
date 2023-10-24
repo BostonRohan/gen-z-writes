188aaeada82d3ddbd3de16478171e93140b142ac
BostonRohan <bostonrohan@gmail.com>
2023-10-24 00:18:18 -0400

    fix sanity client

--- sanity/client.ts
+++ sanity/client.ts
@@ -5,6 +5,7 @@ const client = ({ useCdn }: { useCdn?: boolean }) =>
   createClient({
     projectId,
     dataset,
+    token: process.env.SANITY_API_TOKEN,
     apiVersion, // https://www.sanity.io/docs/api-versioning
     useCdn: useCdn ?? true, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
   });
