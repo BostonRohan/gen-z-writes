21fdd49e84c46bbe0807c6e3beb394db52d0d436
BostonRohan <bostonrohan@gmail.com>
2024-01-28 00:27:26 -0500

    edge function resend

--- app/api/send-email-verify/route.ts
+++ app/api/send-email-verify/route.ts
@@ -3,6 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 import jwt from "jsonwebtoken";
 
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
 const resend = new Resend(process.env.RESEND_TOKEN);
 
 export async function POST(request: NextRequest) {
