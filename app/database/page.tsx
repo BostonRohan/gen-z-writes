30814db616471cf2f163c9cdc55cae67f135c4f8
BostonRohan <bostonrohan@gmail.com>
2024-11-17 01:43:12 -0500

    style: mobile version of banner on database page

--- app/database/page.tsx
+++ app/database/page.tsx
@@ -8,12 +8,12 @@ export default async function Page() {
   return (
     <div className="scroll-mt-24">
       <div className="w-full px-4">
-        <div className="h-12 max-w-4xl text-sm mt-6 rounded-lg mx-auto p-4 bg-accent/60 flex items-center justify-between">
+        <div className="max-w-4xl gap-2 text-sm mt-6 rounded-lg mx-auto p-4 bg-accent/60 flex xs:flex-row flex-col items-center justify-between">
           Interested in being apart of the database?
           <Link
             role="button"
             href="/submit-video"
-            className="bg-primary text-black p-1.5 text-xs rounded-md hover:bg-neutral-300"
+            className="bg-primary text-black p-1.5 text-xs rounded-md hover:bg-neutral-300 xs:w-fit w-full text-center"
           >
             Submit video
           </Link>
