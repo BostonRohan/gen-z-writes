e37b31366fcb22089c47ddec34bd08c2f449bde4
BostonRohan <bostonrohan@gmail.com>
2024-11-17 00:18:17 -0500

    chore: add banner to database page

--- app/database/page.tsx
+++ app/database/page.tsx
@@ -1,11 +1,24 @@
 import VideoGrid from "@/components/VideoGrid";
 import Footer from "@/components/Footer";
 import getVideos from "@/utils/getVideos";
+import Link from "next/link";
 
 export default async function Page() {
   const videos = await getVideos();
   return (
     <div className="scroll-mt-24">
+      <div className="w-full px-4">
+        <div className="h-12 max-w-4xl text-sm mt-6 rounded-lg mx-auto p-4 bg-accent/60 flex items-center justify-between">
+          Interested in being apart of the database?
+          <Link
+            role="button"
+            href="/submit-video"
+            className="bg-primary text-black p-1.5 text-xs rounded-md hover:bg-neutral-300"
+          >
+            Submit video
+          </Link>
+        </div>
+      </div>
       <VideoGrid videos={videos} />
       <Footer />
     </div>
