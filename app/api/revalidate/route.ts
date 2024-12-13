9255c577fdc37ceaf5752b72f54257b3ce67b7d0
BostonRohan <bostonrohan@gmail.com>
2024-12-12 23:42:10 -0500

    feat: revalidate by tags
    chore: add video tag to getVideos query

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,6 +2,10 @@ import { revalidateTag } from "next/cache";
 import { type NextRequest, NextResponse } from "next/server";
 import { parseBody } from "next-sanity/webhook";
 
+type WebhookPayload = {
+  _type: string;
+};
+
 export async function POST(req: NextRequest) {
   try {
     const { body, isValidSignature } = await parseBody<{
@@ -13,8 +17,27 @@ export async function POST(req: NextRequest) {
       return new Response("Invalid Signature", { status: 401 });
     }
 
-    if (!body?._type) {
-      return new Response("Bad Request", { status: 400 });
+    const { isValidSignature, body } = await parseBody<WebhookPayload>(
+      req,
+      process.env.SANITY_REVALIDATE_SECRET,
+    );
+
+    if (!isValidSignature) {
+      const message = "Invalid signature";
+      return new Response(JSON.stringify({ message, isValidSignature, body }), {
+        status: 401,
+      });
+    } else if (!body?._type) {
+      const message = "Bad Request";
+      return new Response(JSON.stringify({ message, body }), { status: 400 });
+    }
+
+    revalidateTag(body._type);
+
+    return NextResponse.json({ body });
+  } catch (err) {
+    if (err instanceof Error) {
+      return new Response(err.message, { status: 500 });
     }
 
     if (body.path) {
