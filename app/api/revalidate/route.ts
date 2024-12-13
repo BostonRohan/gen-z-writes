f6ff74290c588f5e2499c79368b1f09d7a7ad7d7
BostonRohan <bostonrohan@gmail.com>
2024-12-13 00:01:38 -0500

    chore: adjust logic in revalidation to handle slugs

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,37 +2,19 @@ import { revalidateTag } from "next/cache";
 import { type NextRequest, NextResponse } from "next/server";
 import { parseBody } from "next-sanity/webhook";
 
-type WebhookPayload = {
-  _type: string;
-};
-
 export async function POST(req: NextRequest) {
   try {
-    if (!process.env.SANITY_REVALIDATE_SECRET) {
-      return new Response("Missing credentials", { status: 500 });
-    }
-
-    const { isValidSignature, body } = await parseBody<WebhookPayload>(
-      req,
-      process.env.SANITY_REVALIDATE_SECRET,
-    );
+    const { body, isValidSignature } = await parseBody<{
+      _type: string;
+      slug?: string | undefined;
+    }>(req, process.env.SANITY_HOOK_SECRET);
 
     if (!isValidSignature) {
-      const message = "Invalid signature";
-      return new Response(JSON.stringify({ message, isValidSignature, body }), {
-        status: 401,
-      });
-    } else if (!body?._type) {
-      const message = "Bad Request";
-      return new Response(JSON.stringify({ message, body }), { status: 400 });
+      return new Response("Invalid Signature", { status: 401 });
     }
 
-    revalidateTag(body._type);
-
-    return NextResponse.json({ body });
-  } catch (err) {
-    if (err instanceof Error) {
-      return new Response(err.message, { status: 500 });
+    if (!body?._type) {
+      return new Response("Bad Request", { status: 400 });
     }
 
     if (body.slug) {
