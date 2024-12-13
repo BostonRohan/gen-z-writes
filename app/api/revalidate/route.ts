ff5d0c255066f9c47f1e9fbc81812ab79e0dd484
BostonRohan <bostonrohan@gmail.com>
2024-12-12 23:18:02 -0500

    fix: revalidate by path since there are no tags

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: NextRequest) {
     }
 
     if (body.slug) {
-      revalidateTag(`${body._type}:${body.slug}`);
+      revalidatePath(`/${body._type}/${body.slug}`);
     }
 
     revalidateTag(body._type);
