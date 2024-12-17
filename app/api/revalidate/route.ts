0a49d59cc2bb8b902fa4f4b785b7de5b7f3319ea
BostonRohan <bostonrohan@gmail.com>
2024-12-16 21:28:15 -0500

    chore: use slug body for dynamic revalidation

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,15 +1,16 @@
 import { revalidateTag } from "next/cache";
 import { type NextRequest, NextResponse } from "next/server";
 import { parseBody } from "next-sanity/webhook";
+import { Slug } from "sanity";
 
 export async function POST(req: NextRequest) {
   try {
     const { body, isValidSignature } = await parseBody<{
       _type: string;
-      path?: string | undefined;
+      slug: Slug;
     }>(req, process.env.SANITY_HOOK_SECRET);
 
-    console.log("body:", body);
+    console.log({ body });
 
     if (!isValidSignature) {
       return new Response("Invalid Signature", { status: 401 });
@@ -28,10 +29,12 @@ export async function POST(req: NextRequest) {
       return new Response("Bad Request", { status: 400 });
     }
 
-    if (body.path) {
-      console.log(`revalidating tag: ${body._type}:${body.path}`);
-      revalidateTag(`${body._type}:${body.path}`);
+    if (body.slug) {
+      console.log(`revalidating tag: ${body._type}:${body.slug.current}`);
+
+      revalidateTag(`${body._type}:${body.slug.current}`);
     }
+
     console.log(`revalidating tag: ${body._type}`);
 
     revalidateTag(body._type);
