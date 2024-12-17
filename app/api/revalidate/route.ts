74d8e0aacc43fbbcc9626b937a4c5418358ae1a1
Boston <bostonrohan@gmail.com>
2024-12-16 21:36:54 -0500

    feat: revalidate by tags (#63)
    * feat: revalidate by tags
    
    chore: add video tag to getVideos query
    
    * chore: adjust logic in revalidation to handle slugs
    
    * test: log response body
    
    * chore: use slug body for dynamic revalidation
    
    * chore: resolve conflicts
    
    * chore: make slug optional

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,13 +1,16 @@
 import { revalidateTag } from "next/cache";
 import { type NextRequest, NextResponse } from "next/server";
 import { parseBody } from "next-sanity/webhook";
+import { Slug } from "sanity";
 
 export async function POST(req: NextRequest) {
   try {
     const { body, isValidSignature } = await parseBody<{
       _type: string;
-      path?: string | undefined;
+      slug?: Slug;
     }>(req, process.env.SANITY_HOOK_SECRET);
+
+    console.log({ body });
 
     if (!isValidSignature) {
       return new Response("Invalid Signature", { status: 401 });
@@ -17,10 +20,14 @@ export async function POST(req: NextRequest) {
       return new Response("Bad Request", { status: 400 });
     }
 
-    if (body.path) {
-      console.log(`revalidating tag: ${body._type}:${body.path}`);
-      revalidateTag(`${body._type}:${body.path}`);
+    if (body.slug) {
+      console.log(
+        `revalidating dynamic tag: ${body._type}:${body.slug.current}`,
+      );
+
+      revalidateTag(`${body._type}:${body.slug.current}`);
     }
+
     console.log(`revalidating tag: ${body._type}`);
 
     revalidateTag(body._type);
